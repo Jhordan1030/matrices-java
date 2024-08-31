3b30d2521ae545f381813c2e97631c0dbf78ebe7
Jhordan Huera <jhordanhuera2002@gmail.com>
2024-08-31 01:38:39 -0500

    cajero funcionado pero falta corregir errores y mejorar algunas cosas

--- matrices/src/Cajero.java
+++ matrices/src/Cajero.java
@@ -1,2 +1,35 @@
-public class Cajero {
+public class Cajero extends Persona{
+
+    public Cajero(String nombre, String apellido, int edad, double saldo){
+        super(nombre, apellido, edad, saldo);
+    }
+
+    public double Deposito(double monto){
+        if (monto > 0) {
+            this.saldo += monto;
+            System.out.println("Has depositado $" + monto);
+            return saldo;
+
+        }else{
+            System.out.println("Valor ingresado incorrecto");
+            return getSaldo();
+        }
+    }
+
+    public double Retirar(double monto){
+        if(monto > 0 && monto <= getSaldo()){
+            this.saldo -= monto;
+            System.out.println("Has retirado $" + monto);
+            return saldo;
+        }else{
+            System.out.println("Valor ingresado incorrecto");
+            return getSaldo();
+        }
+    }
+
+
+    public String mostrarDatosCajero(){
+        String cadena = super.mostrarDatosPersona() +"";
+        return cadena;
+    }
 }
--- matrices/src/Main.java
+++ matrices/src/Main.java
@@ -3,5 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("ESta es la rama-cajero");
+        Cajero c1;
+        c1 = new Cajero("Jhordan","Huera" , 22, 1000);
+        
+       System.out.println(c1.mostrarDatosCajero());
+        c1.Deposito(500);
+        System.out.println("Saldo actual $" + c1.getSaldo());
+        
+        
+        System.out.println("Retiro");
+        
+
+        c1.Retirar(10);
+        System.out.println(c1.mostrarDatosCajero());
+
     }
 }
