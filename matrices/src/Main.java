d4b28e4d751a59867bf6a854a2ac0814694f8a10
Jhordan Huera <jhordanhuera2002@gmail.com>
2024-09-06 12:18:27 -0500

    creacion de un vector en el cual solo se le inicializa la variable

--- matrices/src/Main.java
+++ matrices/src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         
         System.out.println("Aqui trabajaremos vectores");
+        
+        int [] vectores = {1,2,3,4,5};
+        System.out.println(vectores[3]);
     }
 }
