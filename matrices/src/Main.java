45bb4180c7bd83c356e4931bc0001b021c735329
Jhordan Huera <jhordanhuera2002@gmail.com>
2024-09-07 00:33:44 -0500

    actualizacion

--- matrices/src/Main.java
+++ matrices/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("ESta es la rama main :)");
-=======
-        System.out.println("rama edicion :)");
->>>>>>> rama-edicion
+
+        
+
     }
 }
