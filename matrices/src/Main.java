b7b712728b2368e3bc7345d63423653459ac4e0c
Jhordan Huera <jhordanhuera2002@gmail.com>
2024-09-06 23:28:46 -0500

    llenado de un vector

--- matrices/src/Main.java
+++ matrices/src/Main.java
@@ -18,5 +18,14 @@ public class Main {
         for (int i = 10; i >= 0; i--) {
             System.out.println(i);
         }
+        System.out.println("\nLlenado de vector con el Ciclo For");
+        int[] v = new int[5];
+
+        for (int i = 0; i < v.length; i++) {
+            v[i] = i+1;
+        }
+        for (int i = 0; i < v.length; i++) {
+            System.out.println(v[i]);
+        }
     }
 }
