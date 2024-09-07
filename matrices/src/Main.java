564bf071c51a45a8db02780960daf81ce2cccca4
Jhordan Huera <jhordanhuera2002@gmail.com>
2024-09-07 00:17:33 -0500

    llenado de una matriz y la creacion de un metodo imprimirMatriz

--- matrices/src/Main.java
+++ matrices/src/Main.java
@@ -1,7 +1,48 @@
 public class Main {
+
+    public static void imprimirMatriz(int [][]matriz){
+        int filas = matriz.length;
+        int columnas = matriz[0].length;
+        for (int i = filas -1; i >= 0 ; i--) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
 
         System.out.println("Aqui trabajaremos Matrices");
+        int[][] matriz = new int[6][6];
+        int cont = 1;
 
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                matriz[i][j] = cont;
+                cont++;
+            }
+        }
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print("\t"+ matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Matriz invertida");
+
+        int filas = matriz.length;
+        int columnas = matriz[0].length;
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas / 2; j++) {
+                int temp = matriz[i][j];
+                matriz[i][j] = matriz[i][columnas - 1 - j];
+                matriz[i][columnas - 1 - j] = temp;
+            }
+        }
+
+        imprimirMatriz(matriz);
     }
+
 }
