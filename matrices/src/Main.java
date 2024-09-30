5ecba7bdef2ace361e4b05909a01cd61152ed95c
Jhordan Huera <jhordanhuera2002@gmail.com>
2024-09-30 11:24:13 -0500

    actualizacón

--- matrices/src/Main.java
+++ matrices/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Aqui trabajaremos vectores");
-        Scanner leer = new Scanner();
+        Scanner entrada = new Scanner(System.in);
         int vector1 [] = new int[4];
         vector1[0] =35;
         vector1[1] =28;
@@ -13,9 +13,13 @@ public class Main {
 
         System.out.println(vector1[0]);
         int []vector2 = new int [4];
-
-        for (int i = 0; i < 3; i++) {
-            System.out.println(vector2[]);
+        for (int i = 0; i < vector2.length; i++) {
+            System.out.print("Increse un numero para la posicion " + i + ": ");
+            vector2[i] = entrada.nextInt();
+        }
+        System.out.println("El vector es ");
+        for (int i = 0; i < vector2.length; i++) {
+            System.out.println("Posición " + i + ": " + vector2[i]);
         }
     }
 }
