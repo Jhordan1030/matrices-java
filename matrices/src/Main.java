b0b140aef00db5c638d2ae6e1aba23e7883e65cf
Esteban Huera <esteban123huera@gmail.com>
2024-10-02 08:11:10 -0500

    correccion de errores

--- matrices/src/Main.java
+++ matrices/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("ESta es la rama nueva :)");
-        System.out.println("Nuevo mensaje")
+        System.out.println("Nuevo mensaje");
 
 
     }
