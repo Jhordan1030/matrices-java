fc26ad4859f9886d70fb822a685fd9bad33907d4
Jhordan Huera <jhordanhuera2002@gmail.com>
2024-10-01 21:55:39 -0500

    Actualizacion en imprimir

--- matrices/src/Main.java
+++ matrices/src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -10,8 +11,14 @@ public class Main {
         vector1[1] =28;
         vector1[2] =16;
         vector1[3] =44;
-
+        System.out.println("Imprimir de uno en uno");
         System.out.println(vector1[0]);
+
+        System.out.println("Imprimir todo el vector");
+        for (int i = 0; i < vector1.length; i++) {
+            System.out.println((i + 1) + ": " + vector1[i]);
+        }
+
         int []vector2 = new int [4];
         for (int i = 0; i < vector2.length; i++) {
             System.out.print("Increse un numero para la posicion " + i + ": ");
