fa6bd1d17d207a694110e7d045646da1515056bd
Jhordan Huera <jhordanhuera2002@gmail.com>
2024-10-02 14:39:56 -0500

    ejercicios basicos con for

--- matrices/src/Main.java
+++ matrices/src/Main.java
@@ -1,9 +1,35 @@
 public class Main {
+    public static void Multipicar(int n){
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i + "\t * " + n + " =\t" + (i*n));
+        }
+    }
+    public static void Dividir(int n){
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i + "\t / " + n + " =\t" + (i/n));
+        }
+    }
+    public static void Sumar(int n){
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i + "\t + " + n + " =\t" + (i+n));
+        }
+    }
+    public static void Restar(int n){
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i + "\t - " + n + " =\t" + (i-n));
+        }
+    }
     public static void main(String[] args) {
 
         System.out.println("Esta es la rama for :)");
-
-
-
+        int n = 2;
+        System.out.println("Multiplicar");
+        Multipicar(n);
+        System.out.println("Dividir");
+        Dividir(n);
+        System.out.println("Sumar");
+        Sumar(n);
+        System.out.println("Restar");
+        Restar(n);
     }
 }
