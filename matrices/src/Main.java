7e98ba8edc6661627c3167d2c787c3127114e84a
Jhordan Huera <jhordanhuera2002@gmail.com>
2024-10-02 23:20:12 -0500

    "editado"

--- matrices/src/Main.java
+++ matrices/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("ESta es la rama main :)");
+        System.out.println("Esta es la rama main :)");
 
 
 
